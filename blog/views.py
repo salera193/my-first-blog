a161fa1786dd66450a72ff96c7c7f6dcee11aab5
Your Name <you@example.com>
2020-03-03 17:25:38 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -16,3 +18,47 @@ def post_detail(request, pk):
         'post': post
         }
     return render(request, 'blog/post_detail.html', params)
+
+
+def post_new(request):
+    
+    if request.method =='POST':
+        form = PostForm(request.POST)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            
+            return redirect('post_detail', pk=post.pk)
+        
+    else:
+        form = PostForm()
+        
+    params = {
+        'form':form
+        }
+    return render(request,'blog/post_edit.html', params)
+
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    if request.method=='POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk )
+    else:   
+        form = PostForm(instance=post)
+    
+    params = {
+        'form': form
+        }
+        
+    return render(request, 'blog/post_edit.html', params)
